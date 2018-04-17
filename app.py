affca23a0e3c5902e5cdb471886d60c8b2a1d387
Gabriel Plassard <gabriel.plassard@gmail.com>
2018-04-17 21:53:55 +0200

    jinja template

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello Flask!! (2)'
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
