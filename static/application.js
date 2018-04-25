4c592b5b6be14fb141718090ec2805ac334f4d3c
Gabriel Plassard <gabriel.plassard@gmail.com>
2018-04-25 23:13:18 +0200

    Select 2 inputs

---
+++ static/application.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#func').select2({tags: true, theme: 'classic'});
+    $('#cpd').select2({tags: true, theme: 'classic'});
+    $('#rxn').select2({tags: true, theme: 'classic'});
+    $('#enz').select2({tags: true, theme: 'classic'});
+    $('#taxonomyRank').select2({theme: 'classic'});
+});
