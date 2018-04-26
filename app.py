89e50f70bcdaa1d1297b9dde2b6fb94eb238b41a
Gabriel Plassard <gabriel.plassard@gmail.com>
2018-04-26 13:56:24 +0200

    Search result page

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, g, render_template
+from flask import Flask, g, render_template, request
 from application import database
 
 app = Flask(__name__)
@@ -8,6 +8,22 @@ app = Flask(__name__)
 def index():
     taxonomy_ranks = database.list_taxonomy_ranks()
     return render_template('index.html', taxonomy_ranks=taxonomy_ranks)
+
+
+@app.route('/search')
+def search():
+    func = request.args.get("func")
+    cpd = request.args.get("cpd")
+    rxn = request.args.get("rxn")
+    enz = request.args.get("enz")
+    taxonomy_rank = request.args.get("taxonomyRank")
+    min_score = request.args.get("minScore")
+    max_score = request.args.get("maxScore")
+
+    print(str(request.args.to_dict()))
+
+    return render_template('search.html', func=func, cpd=cpd, rxn=rxn, enz=enz, taxonomy_rank=taxonomy_rank,
+                           min_score=min_score, max_score=max_score)
 
 
 @app.teardown_appcontext
